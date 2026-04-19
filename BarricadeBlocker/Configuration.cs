2fd3dea14092adc2b6eb61e27a2301c6cc8d0c3f
Bagieta <gildia.bee@gmail.com>
2026-04-19 21:24:53 +0200

    Add files via upload

---
+++ BarricadeBlocker/Configuration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Rocket.API;
+
+namespace VehicleBarricadeBlocker
+{
+	// Token: 0x02000002 RID: 2
+	public class Configuration : IRocketPluginConfiguration, IDefaultable
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		public void LoadDefaults()
+		{
+			this.SendWarning = true;
+			this.WarningMessage = "Blocked!";
+			this.AllowedBarricadeIDs = new List<ushort>
+			{
+				123,
+				456
+			};
+			this.AdminBypass = true;
+		}
+
+		// Token: 0x04000001 RID: 1
+		public bool SendWarning;
+
+		// Token: 0x04000002 RID: 2
+		public string WarningMessage;
+
+		// Token: 0x04000003 RID: 3
+		public List<ushort> AllowedBarricadeIDs;
+
+		// Token: 0x04000004 RID: 4
+		public bool AdminBypass;
+	}
+}

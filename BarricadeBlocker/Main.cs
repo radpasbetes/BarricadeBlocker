95d9ff3712ba2f5075021bc07ec1c22ff2cbf352
Bagieta <gildia.bee@gmail.com>
2026-04-19 21:26:46 +0200

    Update Main.cs

--- BarricadeBlocker/Main.cs
+++ BarricadeBlocker/Main.cs
@@ -9,22 +9,18 @@ using UnityEngine;
 
 namespace VehicleBarricadeBlocker
 {
-	// Token: 0x02000003 RID: 3
 	public class Main : RocketPlugin<Configuration>
 	{
-		// Token: 0x06000003 RID: 3 RVA: 0x00002091 File Offset: 0x00000291
 		protected override void Load()
 		{
 			BarricadeManager.onDeployBarricadeRequested = (DeployBarricadeRequestHandler)Delegate.Combine(BarricadeManager.onDeployBarricadeRequested, new DeployBarricadeRequestHandler(this.OnBarricadeDeployRequested));
 		}
 
-		// Token: 0x06000004 RID: 4 RVA: 0x000020B3 File Offset: 0x000002B3
 		protected override void Unload()
 		{
 			BarricadeManager.onDeployBarricadeRequested = (DeployBarricadeRequestHandler)Delegate.Remove(BarricadeManager.onDeployBarricadeRequested, new DeployBarricadeRequestHandler(this.OnBarricadeDeployRequested));
 		}
 
-		// Token: 0x06000005 RID: 5 RVA: 0x000020D8 File Offset: 0x000002D8
 		private void OnBarricadeDeployRequested(Barricade barricade, ItemBarricadeAsset asset, Transform hit, ref Vector3 point, ref float angle_x, ref float angle_y, ref float angle_z, ref ulong owner, ref ulong group, ref bool shouldAllow)
 		{
 			try

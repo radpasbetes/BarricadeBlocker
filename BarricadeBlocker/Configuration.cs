785bf688573746e94e1c1bd8c83d248c231d6de6
Bagieta <gildia.bee@gmail.com>
2026-04-19 21:27:08 +0200

    Update Configuration.cs

--- BarricadeBlocker/Configuration.cs
+++ BarricadeBlocker/Configuration.cs
@@ -4,10 +4,8 @@ using Rocket.API;
 
 namespace VehicleBarricadeBlocker
 {
-	// Token: 0x02000002 RID: 2
 	public class Configuration : IRocketPluginConfiguration, IDefaultable
 	{
-		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
 		public void LoadDefaults()
 		{
 			this.SendWarning = true;
@@ -20,16 +18,12 @@ namespace VehicleBarricadeBlocker
 			this.AdminBypass = true;
 		}
 
-		// Token: 0x04000001 RID: 1
 		public bool SendWarning;
 
-		// Token: 0x04000002 RID: 2
 		public string WarningMessage;
 
-		// Token: 0x04000003 RID: 3
 		public List<ushort> AllowedBarricadeIDs;
 
-		// Token: 0x04000004 RID: 4
 		public bool AdminBypass;
 	}
 }
